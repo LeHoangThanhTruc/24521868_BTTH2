5412bf5274c04c535b72415e223fa4ea5dfa9d41
LeHoangThanhTruc <24521868@gm.uit.edu.vn>
2025-04-08 03:49:14 +0700

    Add files via upload

---
+++ BT1_NgayThangNam_BTTH2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include "NgayThangNam.h"
+using namespace std;
+
+int main()
+{
+    NgayThangNam d;
+    d.Nhap();
+    d.Xuat();
+    return 0;
+}
+/*Input và Output mẫu
+Nhap ngay dinh dang dd/mm/yyyy : 25/10/2025
+Ngay ke tiep : 26/10/2025
+
+Nhap ngay dinh dang dd/mm/yyyy : 29/2/2025
+Ngay khong hop le! Moi nhap lai
+Nhap ngay dinh dang dd/mm/yyyy : 28/2/2025
+Ngay ke tiep : 01/03/2025
+
+Nhap ngay dinh dang dd/mm/yyyy : 31/12/2022
+Ngay ke tiep : 01/01/2023
+*/
