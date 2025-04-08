e342d992fcc9c6f737f9f08a033188fc10a947ae
LeHoangThanhTruc <24521868@gm.uit.edu.vn>
2025-04-08 11:00:44 +0700

    Add files via upload

---
+++ BT4_GioPhut_BTTH2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "GioPhutGiay.h"
+using namespace std;
+
+int main()
+{
+    GioPhutGiay g;
+    g.Nhap();
+    g.Xuat();
+    return 0;
+}
+/*Mẫu Input và Output
+Nhap gio : 23
+Nhap phut : 59
+Nhap giay : 59
+Thoi gian sau khi cong them 1 giay la 0:0:0
+
+Nhap gio : 12
+Nhap phut : 60
+Nhap giay : 60
+Thoi gian nhap khong hop le ! Moi nhap lai
+Nhap gio : 12
+Nhap phut : 59
+Nhap giay : 59
+Thoi gian sau khi cong them 1 giay la 13:0:0
+
+Nhap gio : 15
+Nhap phut : 59
+Nhap giay : 6
+Thoi gian sau khi cong them 1 giay la 15:59:7
+*/
